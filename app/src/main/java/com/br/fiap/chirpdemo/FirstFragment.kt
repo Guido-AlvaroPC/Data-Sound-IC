11cb77b11ca37c92463144564f198936359e8c79
Clauber Souza <clauber.cesario@gmail.com>
2023-07-06 21:38:36 -0300

    add log event

--- app/src/main/java/com/br/fiap/chirpdemo/FirstFragment.kt
+++ app/src/main/java/com/br/fiap/chirpdemo/FirstFragment.kt
@@ -1,5 +1,7 @@
 package com.br.fiap.chirpdemo
 
+import android.content.Context
+import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
@@ -9,6 +11,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import com.br.fiap.chirpdemo.chirp.ChirpSDK
 import com.br.fiap.chirpdemo.chirp.ShortCode
@@ -19,7 +22,11 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import java.io.FileOutputStream
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 import javax.inject.Inject
+
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -35,17 +42,20 @@ class FirstFragment : Fragment() {
     private val chirpEngineListener: ChirpEngineObserver = object : ChirpEngineObserver {
         // from class: io.chirp.sdk.ChirpSDK.5
         // io.chirp.chirpengine.ChirpEngineObserver
+        @RequiresApi(Build.VERSION_CODES.O)
         override fun onChirpReceived(shortCode: ShortCode?) {
             notifyObserversShortCodeReceived(shortCode)
         }
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     fun notifyObserversShortCodeReceived(shortCode: ShortCode?) {
         if (shortCode?.getShortCode().toString().last().equals('1')) {
             lista.add(shortCode?.getShortCode().toString())
             Log.e("Fim de lista",lista.toString())
             val concatenaString =   lista.joinToString("")
             val trocaString = concatenaString.replace("1","")
+            saveFile(this.requireContext(),"log_recebimento.txt")
             activity?.runOnUiThread {
                 binding.msgRecebido.text =  trocaString
             }
@@ -55,6 +65,30 @@ class FirstFragment : Fragment() {
             lista.add(shortCode?.getShortCode().toString())
 
     }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun saveFile(context: Context, filename:String) {
+        try {
+            val outputStream: FileOutputStream = context.openFileOutput(filename, Context.MODE_APPEND)
+            val horario = getCurrentDateTime()
+            val palavra = "$horario\n"
+            outputStream.write(palavra.toByteArray())
+            outputStream.close()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun getCurrentDateTime(): String {
+        val currentDateTime: LocalDateTime = LocalDateTime.now()
+        val formatter: DateTimeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss dd-MM-yyyy")
+
+        return currentDateTime.format(formatter)
+    }
+
+
+
 
     @Inject
     var chirpSDK: ChirpSDK? = null
@@ -73,13 +107,16 @@ class FirstFragment : Fragment() {
 
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         chirpSDK = activity?.baseContext?.let { it1 -> ChirpSDK(it1, KEY, PWD) }
         ChirpEngine().setListener(this.chirpEngineListener)
         binding.sendMsg.setOnClickListener {
+            saveFile(this.requireContext(),"log_envio.txt")
             val repeatableJob = CoroutineScope(Dispatchers.IO).launch {
                 for (palavra in splitString()) {
+
                     chirpSDK?.play(ShortCode(palavra.toString()))
                     delay(2000)
                 }
